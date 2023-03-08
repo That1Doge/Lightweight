9f2c62274b3c8e76b89d8e6c1cf7ae767af2d099
Derek K <djk2529@rit.edu>
2023-03-08 16:14:26 -0500

    Created properties and methods for IMove and IShoot

--- Lightweight/IMove.cs
+++ Lightweight/IMove.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Lightweight
 {
+    public enum Direction
+    { 
+        Left, 
+        Right, 
+        Up, 
+        Down
+    }
     internal interface IMove
     {
+        /// <summary>
+        /// Determines how far an object will move in one Move method call
+        /// </summary>
+        public float Speed { get; }
+
+        /// <summary>
+        /// Move object in the specified direction 
+        /// </summary>
+        public void Move(Direction direction);
     }
 }
