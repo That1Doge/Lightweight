ebc081d2c953e4772a1bb97624379cd87d31a612
Adam Donnelly <and9706@rit.edu>
2023-03-08 14:17:20 -0500

    Updated ITakeDamage interface, implemented it in enemy and player classes, and added a health and damage reduction field to player class along with get and set methods tO work with the damage method

--- Lightweight/Player.cs
+++ Lightweight/Player.cs
@@ -10,6 +10,20 @@ namespace Lightweight
 {
     public class Player : GameObject, IShoot, IMove, ITakeDamage
     {
+        int playerHealth;
+        int playerDefense;
+
+        public int PlayerHealth
+        {
+            get { return playerHealth; }
+            set { playerHealth = value; }
+        }
+
+        public int PlayerDefense
+        {
+            get { return playerDefense; }
+            set { playerDefense = value; }
+        }
 
         public Player(Texture2D texture, Vector2 position)
             : base(texture, position)        
@@ -24,6 +38,12 @@ namespace Lightweight
         public override void Update()
         {
             throw new NotImplementedException();
+        }
+        public void ITakeDamage(int damage, int defense)
+        {
+            //damage taken is reduced by defense of player,
+            //possibly modified by armor or similar attributes
+            this.playerHealth = playerHealth - (damage - defense);
         }
     }
 }
