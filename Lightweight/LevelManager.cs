628a81129f72f9ff3c260a5abf2c170be6f2c648
ryanisnothere <rgn1061@g.rit.edu>
2023-04-12 17:19:50 -0400

    reading from files is now working!!!!!

--- Lightweight/LevelManager.cs
+++ Lightweight/LevelManager.cs
@@ -35,9 +35,9 @@ namespace Lightweight
         public void LoadLevel(string filename) 
         {
             StreamReader input = new StreamReader(filename);
-            string line = input.ReadLine();
+            string line = "";
             yPosTile = 0;
-            while (line != null)
+            while ((line = input.ReadLine()) != null)
             {
                 string[] split = line.Split(',');
 
@@ -60,34 +60,30 @@ namespace Lightweight
                 else if (attempt < 14)
                 {
                     string tilesToRead = line;
-
-                    
-                    for (int i = 0; i < 22; i++) 
+                    for (int i = 1; i < 24; i++) 
                     {
-                        //Add random enemy spawns
-                        switch (tilesToRead[i]) 
+                        floorTiles.Add(new Tile(tileTexture, new Rectangle(0, yPosTile, 32, 32), false));
+                        //TODO -- Add random enemy spawns, to traps 
+                        switch (tilesToRead[i - 1])
                         {
                             case 'X':
-                                if (i == 0) 
-                                {
-                                    floorTiles.Add(new Tile(tileTexture, new Rectangle(0, yPosTile, 32, 32), false));
-                                }
-                                else 
-                                {
-                                    floorTiles.Add(new Tile(tileTexture, new Rectangle(floorTiles[i - 1].X + 32, yPosTile, 32, 32), false));
-                                } 
+                                floorTiles.Add(new Tile(tileTexture, new Rectangle(floorTiles[i - 1].X + 32, yPosTile, 32, 32), false));
                                 break;
                             case 'O':
                                 floorTiles.Add(new Tile(trapTexture, new Rectangle(floorTiles[i - 1].X + 32, yPosTile, 32, 32), true));
                                 break;
                         }
-                    }
 
-                   
+                    }
+                    floorTiles.Add(new Tile(tileTexture, new Rectangle(768, yPosTile, 32, 32), false));
                     yPosTile += 32;
                     attempt++;
                 }
-                line = input.ReadLine();
+                floorTiles.Add(new Tile(tileTexture, new Rectangle(0, yPosTile, 32, 32), false));
+                for (int i = 1; i < 25; i++)
+                {
+                    floorTiles.Add(new Tile(tileTexture, new Rectangle(floorTiles[i - 1].X + 32, yPosTile, 32, 32), false));
+                }
             }
             input.Close();
         }
